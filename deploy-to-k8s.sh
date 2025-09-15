cc6fc8e800bcb5317356fa13fc7e7f03d40e0973
theeGwandaru <theeGwandaru@email.com>
2025-09-15 20:51:49 +0300

    Switch from nodePorts to ingress

--- deploy-to-k8s.sh
+++ deploy-to-k8s.sh
@@ -9,6 +9,9 @@ minikube start --cpus=4
 echo "Enabling minikube metrics-server"
 minikube addons enable metrics-server
 
+echo "Enable ingress"
+minikube addons enable ingress
+
 echo "loading saccomizi/user-service image"
 minikube image load saccomizi/user-service
 
@@ -27,8 +30,12 @@ kubectl apply -k ./k8s/
 echo "starting minikube dashboard. It will automatically open a browser window."
 minikube dashboard
 
-# To get your node's ip address
+# To get your node's ip address <node-ip-address>
 # kubectl get node minikube -o=jsonpath='{range .status.addresses[*]}{.type}{"\t"}{.address}{"\n"}'
 
-# To get kafka broker's port
+# To get kafka broker's port. The <node port>
 # kubectl get service saccomizi-cluster-kafka-external-bootstrap -o=jsonpath='{.spec.ports[0].nodePort}{"\n"}'
+
+# To view kafka messages for the deposit-account-topic
+# First have a kafka installation and cd into it's directory
+# /bin/kafka-console-consumer.sh --topic deposit-account-topic --from-beginning --bootstrap-server <node-ip-address>:<node port>
